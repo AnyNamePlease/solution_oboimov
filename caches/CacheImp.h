32d309e8d5cbeb2c256d1b7108b059352af87ef5
Konstantin <kostya.obojmov@mail.ru>
2025-12-17 16:32:23 +0300

    didn't notice the base class file

--- caches/CacheImp.h
+++ caches/CacheImp.h
@@ -7,7 +7,7 @@
 template<typename Key, typename Value>
 class ExampleCache : public ICache<Key, Value> {
 public:
-    explicit ExampleCache(size_t capacity) : capacity_(capacity) {
+    explicit ExampleCache(std::size_t capacity) : capacity_(capacity) {
         if (capacity_ == 0) {
             throw std::invalid_argument("Capacity must be positive");
         }
@@ -16,10 +16,13 @@ public:
     void set(const Key& key, const Value& value) override {
         auto it = cache_map_.find(key);
         if (it != cache_map_.end()) {
+            // Обновляем значение и перемещаем в начало
             it->second->second = value;
             cache_list_.splice(cache_list_.begin(), cache_list_, it->second);
         } else {
+            // Нужно вставить новый элемент
             if (cache_list_.size() >= capacity_) {
+                // Удаляем самый старый (последний в списке)
                 auto last = cache_list_.end();
                 --last;
                 cache_map_.erase(last->first);
@@ -33,14 +36,15 @@ public:
     Value get(const Key& key) override {
         auto it = cache_map_.find(key);
         if (it != cache_map_.end()) {
+            // Делаем элемент "недавно использованным"
             cache_list_.splice(cache_list_.begin(), cache_list_, it->second);
             return it->second->second;
         }
-        return Value{};
+        return Value{}; // возвращаем дефолтное значение, как требует интерфейс
     }
 
     bool contains(const Key& key) const override {
-        return cache_map_.count(key) > 0;
+        return cache_map_.find(key) != cache_map_.end();
     }
 
     void clear() override {
@@ -48,13 +52,13 @@ public:
         cache_list_.clear();
     }
 
-    size_t size() const override {
+    std::size_t size() const override {
         return cache_list_.size();
     }
 
 private:
     using List = std::list<std::pair<Key, Value>>;
-    size_t capacity_;
+    std::size_t capacity_;
     List cache_list_;
     std::unordered_map<Key, typename List::iterator> cache_map_;
 };
